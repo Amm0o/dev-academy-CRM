40fb89d8e470247c353818cf2708abce80192814
anoliveira_linux_laptop <anoliveira@microsoft.com>
2025-05-06 14:30:07 +0100

    Advancing with the setup script

--- CRM/scripts/setup_dev_env.sh
+++ CRM/scripts/setup_dev_env.sh
@@ -6,10 +6,72 @@ sleep 2
 echo  -n "Do you want to continue? (y/N)"
 read answer
 
-if [[ "$answer" == "y" || "$answer" == "Y"]]; then
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "Proceeding with installation..."
     sleep 1
+    # start calling functions
+    check_root
+    installing_git
+    install_dotnet
 else
     echo "exiting installation"
     exit 1
 fi
+
+
+# Function to check if running as root
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "This script needs to be ran as root"
+        return 1
+    fi
+}
+
+# Function to install GIT
+installing_git() {
+    echo "Installing git..."
+    sleep 1
+
+    # Check if package is installed
+    if pacman -Qi "git" &> /dev/null; then
+        echo "Git is already installed... proceeding to next step"
+        return 0
+    fi
+
+    # Attempt to install package
+    echo "Installing GIT since it's not installed already"
+    # Update repos
+    pacman -Sy
+    # Now install git
+    pacman -S --noconfirm git
+
+    if [ $? -eq 0 ]; then
+        echo "Git installed"
+        version_git=$(git --version)
+        printf "Git version $version_git"
+    else
+        echo "Failed to install git code $?"
+        return 1
+    fi
+
+}
+
+install_dotnet() {
+
+    echo "Installing .Net 8.0"
+    # Intall the .Net SDK
+    pacman -S dotnet-sdk-8.0
+    check_if_installed ".Net 8.0"
+
+}
+
+# Logger function to check is something installed
+check_if_installed() {
+    echo "Checking if $1 is installed..."
+    if [ $? -eq 0 ]; then
+        echo "$1 is installed"
+    else 
+      echo "$1 is not installed"
+      return 1
+    fi
+}
